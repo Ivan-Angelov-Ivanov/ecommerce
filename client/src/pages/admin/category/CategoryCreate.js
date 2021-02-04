e353c1d182bb2b435b14e9e51e67268536cda4e7
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-04 21:30:36 +0200

    Added create category component

---
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import {
+  createCategory,
+  getCategories,
+  removeCategory,
+} from "../../../functions/category";
+import CategoryForm from "../../../components/forms/CategoryForm";
+import LocalSearch from "../../../components/forms/LocalSearch";
+
+const CategoryCreate = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = () =>
+    getCategories().then((category) => setCategories(category.data));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    createCategory({ name }, user.token)
+      .then((res) => {
+        setLoading(false);
+        setName("");
+        toast.success(`"${res.data.name}" is created successfully!`);
+        loadCategories();
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+        if (error.response.status === 400) toast.error(error.response.data);
+      });
+  };
+
+  const handleRemove = async (slug) => {
+    if (window.confirm("Delete?")) {
+      setLoading(true);
+      removeCategory(slug, user.token)
+        .then((res) => {
+          setLoading(false);
+          toast.error(`${res.data.name} deleted!`);
+          loadCategories();
+        })
+        .catch((error) => {
+          setLoading(false);
+          if (error.response.status === 400) {
+            toast.error(error.response.data);
+          }
+        });
+    }
+  };
+
+  //filtering based on keyword
+  const searched = (keyword) => (category) =>
+    category.name.toLowerCase().includes(keyword);
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
+        </div>
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>Create category</h4>
+          )}
+          <CategoryForm
+            handleSubmit={handleSubmit}
+            name={name}
+            setName={setName}
+          />
+
+          <LocalSearch keyword={keyword} setKeyword={setKeyword} />
+
+          {categories.filter(searched(keyword)).map((category) => (
+            <div className="alert alert-secondary" key={category._id}>
+              {category.name}
+              <span
+                onClick={() => handleRemove(category.slug)}
+                className="btn btn-sm float-right"
+              >
+                <DeleteOutlined className="text-danger" />
+              </span>
+              <Link to={`/admin/category/${category.slug}`}>
+                <span className="btn btn-sm float-right">
+                  <EditOutlined className="text-warning" />
+                </span>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryCreate;
