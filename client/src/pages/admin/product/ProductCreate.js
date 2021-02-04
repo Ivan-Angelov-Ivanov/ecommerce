bfaad7907e4460099d1d74acb6a250b32afacf87
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-04 21:31:49 +0200

    Added component for creating products

---
+++ client/src/pages/admin/product/ProductCreate.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { createProduct } from "../../../functions/product";
+import ProductCreateForm from "../../../components/forms/ProductCreateForm";
+import { getCategories, getCategorySubs } from "../../../functions/category";
+import FileUpload from "../../../components/forms/FileUpload";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const initialState = {
+  title: "Macbook Pro",
+  description: "This is the best Apple product",
+  price: "4500",
+  categories: [],
+  category: "",
+  subs: [],
+  shipping: "Yes",
+  quantity: "50",
+  images: [],
+  colors: ["Black", "Brown", "Silver", "White", "Blue"],
+  brands: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
+  color: "White",
+  brand: "Apple",
+};
+
+const ProductCreate = () => {
+  const [values, setValues] = useState(initialState);
+  const [subOptions, setSubOptions] = useState([]);
+  const [showSub, setShowSub] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user } = useSelector((state) => ({ ...state }));
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = () =>
+    getCategories().then((category) =>
+      setValues({ ...values, categories: category.data })
+    );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createProduct(values, user.token)
+      .then((res) => {
+        console.log(res);
+        window.alert(`${res.data.title} is created successfully!`);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
+        // if (error.response.status === 400) toast.error(error.response.data);
+        toast.error(error.response.data.error);
+      });
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleCategoryChange = (e) => {
+    e.preventDefault();
+    setValues({ ...values, subs: [], category: e.target.value });
+    getCategorySubs(e.target.value).then((res) => {
+      setSubOptions(res.data);
+    });
+    setShowSub(true);
+  };
+
+  return (
+    <div className="conatiner-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
+        </div>
+
+        <div className="col-md-10">
+          <h4>Product create</h4>
+          <hr />
+          {loading ? <LoadingOutlined className="text-danger h2" /> : ""}
+          <div className="p-3">
+            <FileUpload
+              values={values}
+              setValues={setValues}
+              setLoading={setLoading}
+            />
+          </div>
+
+          <ProductCreateForm
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+            values={values}
+            setValues={setValues}
+            handleCategoryChange={handleCategoryChange}
+            subOptions={subOptions}
+            showSub={showSub}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCreate;
