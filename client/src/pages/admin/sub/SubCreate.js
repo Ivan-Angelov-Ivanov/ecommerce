f3ef65518c3677d5d1172aaa87c28fcc256f8137
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-04 21:32:27 +0200

    Added component for creating sub categories

---
+++ client/src/pages/admin/sub/SubCreate.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from "react";
+import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { createSub, getSub, getSubs, removeSub } from "../../../functions/sub";
+import { getCategories } from "../../../functions/category";
+import CategoryForm from "../../../components/forms/CategoryForm";
+import LocalSearch from "../../../components/forms/LocalSearch";
+
+const SubCreate = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [subs, setSubs] = useState([]);
+  const [category, setCategory] = useState("");
+  const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    loadCategories();
+    loadSubs();
+  }, []);
+
+  const loadCategories = () =>
+    getCategories().then((category) => setCategories(category.data));
+
+  const loadSubs = () => getSubs().then((sub) => setSubs(sub.data));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    createSub({ name, parent: category }, user.token)
+      .then((res) => {
+        setLoading(false);
+        setName("");
+        toast.success(`"${res.data.name}" is created successfully!`);
+        loadSubs();
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+        if (error.response.status === 400) toast.error(error.response.data);
+      });
+  };
+
+  const handleRemove = async (slug) => {
+    if (window.confirm("Delete?")) {
+      setLoading(true);
+      removeSub(slug, user.token)
+        .then((res) => {
+          setLoading(false);
+          toast.error(`${res.data.name} deleted!`);
+          loadSubs();
+        })
+        .catch((error) => {
+          setLoading(false);
+          if (error.response.status === 400) {
+            toast.error(error.response.data);
+          }
+        });
+    }
+  };
+
+  //filtering based on keyword
+  const searched = (keyword) => (category) =>
+    category.name.toLowerCase().includes(keyword);
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
+        </div>
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>Create sub category</h4>
+          )}
+
+          <div className="form-group">
+            <label>Parent category</label>
+            <select
+              name="category"
+              className="form-control"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option>Please Select</option>
+              {categories.length > 0 &&
+                categories.map((category) => (
+                  <option key={category._id} value={category._id}>
+                    {category.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+          <CategoryForm
+            handleSubmit={handleSubmit}
+            name={name}
+            setName={setName}
+          />
+
+          <LocalSearch keyword={keyword} setKeyword={setKeyword} />
+
+          {subs.filter(searched(keyword)).map((sub) => (
+            <div className="alert alert-secondary" key={sub._id}>
+              {sub.name}
+              <span
+                onClick={() => handleRemove(sub.slug)}
+                className="btn btn-sm float-right"
+              >
+                <DeleteOutlined className="text-danger" />
+              </span>
+              <Link to={`/admin/sub/${sub.slug}`}>
+                <span className="btn btn-sm float-right">
+                  <EditOutlined className="text-warning" />
+                </span>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubCreate;
