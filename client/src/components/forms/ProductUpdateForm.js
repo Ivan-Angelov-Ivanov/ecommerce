51b194d87b16770d17a4e1da36703c6b437d2a4b
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-05 18:47:40 +0200

    Added update product component

---
+++ client/src/components/forms/ProductUpdateForm.js
@@ -0,0 +1,167 @@
+import React from "react";
+import { Select } from "antd";
+
+const { Option } = Select;
+
+const ProductUpdateForm = ({
+  handleSubmit,
+  handleChange,
+  values,
+  setValues,
+  categories,
+  subOptions,
+  handleCategoryChange,
+  arrayOfSubs,
+  setArrayOfSubs,
+  selectedCategory,
+}) => {
+  // destructure
+  const {
+    title,
+    description,
+    price,
+    category,
+    subs,
+    shipping,
+    quantity,
+    images,
+    colors,
+    brands,
+    color,
+    brand,
+  } = values;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Title</label>
+        <input
+          type="text"
+          name="title"
+          className="form-control"
+          value={title}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Description</label>
+        <input
+          type="text"
+          name="description"
+          className="form-control"
+          value={description}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Price</label>
+        <input
+          type="number"
+          name="price"
+          className="form-control"
+          value={price}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Shipping</label>
+        <select
+          value={shipping === "Yes" ? "Yes" : "No"}
+          name="shipping"
+          className="form-control"
+          onChange={handleChange}
+        >
+          <option value="No">No</option>
+          <option value="Yes">Yes</option>
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label>Quantity</label>
+        <input
+          type="number"
+          name="quantity"
+          className="form-control"
+          value={quantity}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <label>Color</label>
+        <select
+          value={color}
+          name="color"
+          className="form-control"
+          onChange={handleChange}
+        >
+          {colors.map((color) => (
+            <option key={color} value={color}>
+              {color}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label>Brand</label>
+        <select
+          value={brand}
+          name="brand"
+          className="form-control"
+          onChange={handleChange}
+        >
+          {brands.map((brand) => (
+            <option key={brand} value={brand}>
+              {brand}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label>Category</label>
+        <select
+          name="category"
+          className="form-control"
+          onChange={handleCategoryChange}
+          value={selectedCategory ? selectedCategory : category._id}
+        >
+
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option key={category._id} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+        </select>
+      </div>
+
+      <div>
+        <label>Sub Categories</label>
+        <Select
+          mode="multiple"
+          style={{ width: "100%" }}
+          placeholder="Please select"
+          onChange={(value) => setArrayOfSubs(value)}
+          value={arrayOfSubs}
+        >
+          {subOptions.length &&
+            subOptions.map((sub) => (
+              <Option key={sub._id} value={sub._id}>
+                {sub.name}
+              </Option>
+            ))}
+        </Select>
+      </div>
+
+      <br />
+      <button className="btn btn-outline-info">Save</button>
+    </form>
+  );
+};
+
+export default ProductUpdateForm;
