70c66adf9b0030a9d4fd0a8bacad4fbd096371ff
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-05 18:45:45 +0200

    User friendly design for products

---
+++ client/src/components/cards/AdminProductCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Card } from "antd";
+import laptop from "../../images/laptop-pic-1.jpg";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
+const { Meta } = Card;
+
+const AdminProductCard = ({ product, handleRemove }) => {
+  // destructure
+  const { title, description, images, slug } = product;
+
+  return (
+    <Card
+      cover={
+        <img
+          alt={title}
+          src={images && images.length ? images[0].url : laptop}
+          style={{ height: "150px", objectFit: "cover" }}
+          className="p-1"
+        />
+      }
+      actions={[
+        <Link to={`/admin/product/${slug}`}>
+          <EditOutlined className="text-warning" />
+        </Link>,
+        <DeleteOutlined
+          onClick={() => handleRemove(slug)}
+          className="text-danger"
+        />,
+      ]}
+    >
+      <Meta
+        title={title}
+        description={`${description && description.substr(0, 50)}...`}
+      />
+    </Card>
+  );
+};
+
+export default AdminProductCard;
