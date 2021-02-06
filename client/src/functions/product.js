85f2ac67cc2ea53ac7de470a099493be76806cb3
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-07 00:14:50 +0200

    Added rating and related requests

--- client/src/functions/product.js
+++ client/src/functions/product.js
@@ -47,3 +47,21 @@ export const getProducts = async (sort, order, page) => {
 export const getProductsCount = async () => {
   return await axios.get(`${process.env.REACT_APP_API}/products/total`);
 };
+
+export const productStar = async (productId, star, authtoken) => {
+  return await axios.put(
+    `${process.env.REACT_APP_API}/product/star/${productId}`,
+    { star },
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+};
+
+export const getRelated = async (productId) => {
+  return await axios.get(
+    `${process.env.REACT_APP_API}/product/related/${productId}`
+  );
+};
