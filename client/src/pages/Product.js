bb0dbf6635f48e5b08e993bc74604eb82fb6ec21
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-07 00:01:38 +0200

    Added ratings at product and home page

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,30 +1,73 @@
 import React, { useEffect, useState } from "react";
-import { getProduct } from "../functions/product";
-import SingleProduct from "../components/cards/SingleProduct"
+import { getProduct, productStar, getRelated } from "../functions/product";
+import SingleProduct from "../components/cards/SingleProduct";
+import { useSelector } from "react-redux";
+import ProductCard from "../components/cards/ProductCard";
 
 const Product = ({ match }) => {
   const [product, setProduct] = useState("");
+  const [star, setStar] = useState(0);
+  const [related, setRelated] = useState([]);
+
   const { slug } = match.params;
+  // redux
+  const { user } = useSelector((state) => ({ ...state }));
 
   useEffect(() => {
     loadSingleProduct();
   }, [slug]);
 
+  useEffect(() => {
+    if (product.ratings && user) {
+      let existingRatingObject = product.ratings.find(
+        (element) => element.postedBy.toString() === user._id.toString()
+      );
+      existingRatingObject && setStar(existingRatingObject.star);
+    }
+  }, []);
+
   const loadSingleProduct = () => {
-    getProduct(slug).then((res) => setProduct(res.data));
+    getProduct(slug).then((res) => {
+      setProduct(res.data);
+      // load related
+      getRelated(res.data._id).then((res) => setRelated(res.data));
+    });
+  };
+
+  const onStarClick = (newRating, name) => {
+    setStar(newRating);
+    productStar(name, newRating, user.token).then((res) => {
+      console.log(res.data);
+      loadSingleProduct(); // if you want to show updated rating in real time
+    });
   };
 
   return (
     <div className="container-fluid">
       <div className="row pt-4">
-          <SingleProduct product={product} />
+        <SingleProduct
+          product={product}
+          onStarClick={onStarClick}
+          star={star}
+        />
       </div>
       <div className="row">
         <div className="col text-center pt-5 pb-5">
-            <hr/>
-            <h4>Related products</h4>
-            <hr/>
+          <hr />
+          <h4>Related products</h4>
+          <hr />
         </div>
+      </div>
+      <div className="row pb-5">
+        {related.length ? (
+          related.map((r) => (
+            <div key={r._id} className="col-md-4">
+              <ProductCard product={r} />
+            </div>
+          ))
+        ) : (
+          <div className="text-center col">No products found</div>
+        )}
       </div>
     </div>
   );
