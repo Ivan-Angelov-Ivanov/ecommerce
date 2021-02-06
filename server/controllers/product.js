69a6d4f6d5f579800b1478b4e7d02de6b623807e
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-06 23:56:29 +0200

    Added rating and related routes and functions

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,5 +1,6 @@
 const Product = require("../models/product");
 const slugify = require("slugify");
+const User = require("../models/user");
 
 exports.create = async (req, res) => {
   try {
@@ -107,4 +108,61 @@ exports.list = async (req, res) => {
 exports.productsCount = async (req, res) => {
   let total = await Product.find({}).estimatedDocumentCount().exec();
   res.json(total);
+};
+
+exports.productStar = async (req, res) => {
+  const product = await Product.findById(req.params.productId).exec();
+  const user = await User.findOne({ email: req.user.email }).exec();
+  const { star } = req.body;
+
+  // who is updating?
+  // check if currently logged in user have already added rating to this product?
+  let existingRatingObject = product.ratings.find(
+    (element) => element.postedBy.toString() === user._id.toString()
+  );
+
+  if (existingRatingObject === undefined) {
+    // if user havent left rating yet, push it
+    let ratingAdded = await Product.findByIdAndUpdate(
+      product._id,
+      {
+        $push: {
+          ratings: {
+            star: star,
+            postedBy: user._id,
+          },
+        },
+      },
+      { new: true }
+    ).exec();
+    console.log(ratingAdded);
+    res.json(ratingAdded);
+  } else {
+    // if use have already left rating, update it
+    const ratingUpdated = await Product.updateOne(
+      {
+        ratings: { $elemMatch: existingRatingObject },
+      },
+      { $set: { "ratings.$.star": star } },
+      { new: true }
+    ).exec();
+    console.log("Rating Updated", ratingUpdated);
+    res.json(ratingUpdated);
+  }
+};
+
+exports.listRelated = async (req, res) => {
+  const product = await Product.findById(req.params.productId).exec();
+
+  const related = await Product.find({
+    _id: { $ne: product._id },
+    category: product.category,
+  })
+    .limit(3)
+    .populate("category")
+    .populate("subs")
+    .populate("postedBy")
+    .exec();
+
+  res.json(related);
 };
