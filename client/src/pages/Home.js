e2f498580bb071a2a1d1e830d55c9fabc5e3b7ef
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-06 12:43:56 +0200

    imported new arrivals and best sellers

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
+import Jumbotron from "../components/cards/Jumbotron";
+import NewArrivals from "../components/home/NewArrivals";
+import BestSellers from "../components/home/BestSellers";
 
-const Home = () =>  (
-  <div>
-    <p>react home</p>
-  </div>
-);
+const Home = () => {
+  return (
+    <>
+      <div className="jumbotron text-danger text-center h1 font-weight-bold">
+        <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
+      </div>
 
+      <h4 className="text-center p-3 mt-5 mb-5 display-3 jumbotron">
+        New Arrivals
+      </h4>
+      <NewArrivals />
+      <br />
+      <br />
+      <h4 className="text-center p- 3 mt-5 mb-5 display-3 jumbotron">
+        Best Sellers
+      </h4>
+      <BestSellers />
+    </>
+  );
+};
 
 export default Home;
