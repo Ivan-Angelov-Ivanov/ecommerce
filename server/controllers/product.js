9ad07e9970d0060b6c370d14b76366f35708b2b3
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-06 12:52:25 +0200

    added functions for counting and listing

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -63,3 +63,48 @@ exports.update = async (req, res) => {
     return res.status(400).send("Product Update failed");
   }
 };
+
+// Without pagination
+// exports.list = async (req, res) => {
+//   try {
+//     // createdAt/updatedAt, desc/asc, limit
+//     const { sort, order, limit } = req.body;
+//     const products = await Product.find({})
+//       .populate("category")
+//       .populate("subs")
+//       .sort([[sort, order]])
+//       .limit(limit)
+//       .exec();
+
+//     res.json(products);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// With pagination
+exports.list = async (req, res) => {
+  try {
+    // createdAt/updatedAt, desc/asc, limit
+    const { sort, order, page } = req.body;
+    const currentPage = page || 1;
+    const perPage = 3;
+
+    const products = await Product.find({})
+      .skip((currentPage - 1) * perPage)
+      .populate("category")
+      .populate("subs")
+      .sort([[sort, order]])
+      .limit(perPage)
+      .exec();
+
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.productsCount = async (req, res) => {
+  let total = await Product.find({}).estimatedDocumentCount().exec();
+  res.json(total);
+};
