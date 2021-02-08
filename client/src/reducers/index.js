027d5ab03e63222a5fef973dc7880cb3b281a287
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-08 13:46:15 +0200

    Added cart and drawer to rootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import { searchReducer } from "./searchReducer";
 import { userReducer } from "./userReducer";
+import { cartReducer } from "./cartReducer";
+import { drawerReducer } from "./drawerReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
   search: searchReducer,
+  cart: cartReducer,
+  drawer: drawerReducer,
 });
 
 export default rootReducer;
