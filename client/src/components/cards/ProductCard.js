633d677633b149424ee9bd47805684e56f2a7961
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-08 13:47:06 +0200

    Showing drawer when adding a product

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,14 +1,56 @@
-import React from "react";
-import { Card } from "antd";
+import React, { useState } from "react";
+import { Card, Tooltip } from "antd";
 import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import laptop from "../../images/laptop-pic-1.jpg";
 import { showAverage } from "../../functions/rating";
+import _ from "lodash";
+import { useSelector, useDispatch } from "react-redux";
 
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
+  const [tooltip, setTooltip] = useState("Click to add");
   const { title, description, images, slug } = product;
+
+  // redux
+  const { user, cart } = useSelector((state) => ({ ...state }));
+  const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    // create cart array
+    let cart = [];
+    if (typeof window !== "undefined") {
+      // if cart is in local storage GET it
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      // push new product to cart
+      cart.push({
+        ...product,
+        count: 1,
+      });
+      // remove duplicates
+      let unique = _.uniqWith(cart, _.isEqual);
+      // save to local storage
+      // console.log("Unique", unique);
+      localStorage.setItem("cart", JSON.stringify(unique));
+      // show tooltip
+      setTooltip("Added");
+
+      // add to redux state
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: unique,
+      });
+      //show cart items in side drawer
+      dispatch({
+        type: "SET_VISIBLE",
+        payload: true,
+      });
+    }
+  };
+
   return (
     <div>
       <>
@@ -30,10 +72,12 @@ const ProductCard = ({ product }) => {
             <Link to={`/product/${slug}`}>
               <EyeOutlined className="text-info" /> <br /> View Product
             </Link>,
-            <>
-              <ShoppingCartOutlined className="text-success" /> <br /> Add to
-              cart
-            </>,
+            <Tooltip title={tooltip}>
+              <a onClick={handleAddToCart}>
+                <ShoppingCartOutlined className="text-success" /> <br /> Add to
+                cart
+              </a>
+            </Tooltip>,
           ]}
         >
           <Meta
