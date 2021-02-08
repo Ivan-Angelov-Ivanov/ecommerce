6f2aaff468627b40011f82c0aa0270fa7324e3b5
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-08 13:41:33 +0200

    Added Cart page with reducer

---
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -0,0 +1,146 @@
+import React from "react";
+import ModalImage from "react-modal-image";
+import laptop from "../../images/laptop-pic-1.jpg";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import {
+  CheckCircleOutlined,
+  CloseCircleOutlined,
+  CloseOutlined,
+} from "@ant-design/icons";
+
+const ProductCardInCheckout = ({ product }) => {
+  const colors = ["Black", "Brown", "Silver", "White", "Blue"];
+  const dispatch = useDispatch();
+
+  const handleColorChange = (e) => {
+    console.log("color changed", e.target.value);
+
+    let cart = [];
+    if (typeof window !== undefined) {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      cart.map((cartProduct, i) => {
+        if (cartProduct._id === product._id) {
+          cart[i].color = e.target.value;
+        }
+      });
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+  };
+
+  const handleQualityChange = (e) => {
+    console.log("Available quantity", product.quantity);
+    let count = e.target.value < 1 ? 1 : e.target.value;
+    let cart = [];
+
+    if (count > product.quantity) {
+      toast.error(`Max available quantity: ${product.quantity}`);
+      return;
+    }
+    if (typeof window !== undefined) {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      cart.map((cartProduct, i) => {
+        if (cartProduct._id === product._id) {
+          cart[i].count = count;
+        }
+      });
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+  };
+
+  const handleRemove = () => {
+    let cart = [];
+
+    if (typeof window !== undefined) {
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+
+      cart.map((cartProduct, i) => {
+        if (cartProduct._id === product._id) {
+          cart.splice(i, 1);
+        }
+      });
+      localStorage.setItem("cart", JSON.stringify(cart));
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: cart,
+      });
+    }
+  };
+
+  return (
+    <tbody>
+      <tr>
+        <td>
+          <div style={{ width: "100px", height: "auto" }}>
+            {product.images.length ? (
+              <ModalImage
+                small={product.images[0].url}
+                large={product.images[0].url}
+              />
+            ) : (
+              <ModalImage small={laptop} large={laptop} />
+            )}
+          </div>
+        </td>
+        <td>{product.title}</td>
+        <td>{product.price}</td>
+        <td>{product.brand}</td>
+        <td>
+          <select
+            onChange={handleColorChange}
+            name="color"
+            className="form-control"
+          >
+            {product.color ? (
+              <option>{product.color}</option>
+            ) : (
+              <option>Select</option>
+            )}
+            {colors
+              .filter((color) => color !== product.color)
+              .map((color) => (
+                <option key={color}>{color}</option>
+              ))}
+          </select>
+        </td>
+        <td className="text-center">
+          <input
+            type="number"
+            className="form-control"
+            value={product.count}
+            onChange={handleQualityChange}
+          />
+        </td>
+        <td className="text-center">
+          {product.shipping === "Yes" ? (
+            <CheckCircleOutlined className="text-success" />
+          ) : (
+            <CloseCircleOutlined className="text-danger" />
+          )}
+        </td>
+        <td className="text-center">
+          <CloseOutlined
+            onClick={handleRemove}
+            className="text-danger pointer"
+          />
+        </td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default ProductCardInCheckout;
---
+++ client/src/pages/Cart.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import ProductCardInCheckout from "../components/cards/ProductCardInCheckout";
+
+const Cart = () => {
+  const { cart, user } = useSelector((state) => ({ ...state }));
+  const dispatch = useDispatch();
+
+  const getTotal = () => {
+    return cart.reduce((currentValue, nextValue) => {
+      return currentValue + nextValue.count * nextValue.price;
+    }, 0);
+  };
+
+  const saveOrderToDB = () => {
+    //
+  };
+
+  const showCartItems = () => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Image</th>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+          <th scope="col">Remove</th>
+        </tr>
+      </thead>
+      {cart.map((product) => (
+        <ProductCardInCheckout key={product._id} product={product} />
+      ))}
+    </table>
+  );
+
+  return (
+    <div className="container-fluid pt-2">
+      <div className="row">
+        <div className="col-md-8">
+          <h4>
+            Cart / {cart.length !== 1 ? `${cart.length} Products` : "1 Product"}
+          </h4>
+          {!cart.length ? (
+            <p>
+              No products has been added.{" "}
+              <Link to="/shop">Continue Shopping</Link>
+            </p>
+          ) : (
+            showCartItems()
+          )}
+        </div>
+        <div className="col-md-4">
+          <h4>Order Summary</h4>
+          <hr />
+          <p>Products</p>
+          {cart.map((product, index) => (
+            <div key={index}>
+              <p>
+                {product.title} x {product.count} = $
+                {product.price * product.count}
+              </p>
+            </div>
+          ))}
+          <hr />
+          Total: <b>${getTotal()}</b>
+          <hr />
+          {user ? (
+            <button
+              onClick={saveOrderToDB}
+              className="btn btn-sm btn-primary mt-2"
+              disabled={!cart.length}
+            >
+              Proceed to Checkout
+            </button>
+          ) : (
+            <button className="btn btn-sm btn-primary mt-2">
+              <Link
+                to={{
+                  pathname: "/login",
+                  state: { from: "cart" },
+                }}
+              >
+                Login to Checkout
+              </Link>
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
