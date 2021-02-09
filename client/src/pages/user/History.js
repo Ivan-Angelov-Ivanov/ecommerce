12d434cdc90b17f16c221f50214db36e4c50215d
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-09 13:08:57 +0200

    Showing user orders

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,15 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserNav from "../../components/nav/UserNav";
+import { getUserOrders } from "../../functions/user";
+import { useSelector, useDispatch } from "react-redux";
+import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
+import { toast } from "react-toastify";
+import ShowPaymentInfo from "../../components/cards/ShowPaymentInfo";
 
-const History = () => (
-  <div className="container-fluid">
-    <div className="row">
-      <div className="col-md-2">
-        <UserNav />
+const History = () => {
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadUserOrders();
+  }, []);
+
+  const loadUserOrders = () =>
+    getUserOrders(user.token).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data);
+    });
+
+  const showOrderInTable = (order) => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
+      <tbody>
+        {order.products.map((product, i) => (
+          <tr key={i}>
+            <td>
+              <b>{product.product.title}</b>
+            </td>
+            <td>
+              <b>{product.product.price}</b>
+            </td>
+            <td>
+              <b>{product.product.brand}</b>
+            </td>
+            <td>
+              <b>{product.color}</b>
+            </td>
+            <td>
+              <b>{product.count}</b>
+            </td>
+            <td>
+              <b>
+                {product.product.shipping === "Yes" ? (
+                  <CheckCircleOutlined className="text-success" />
+                ) : (
+                  <CloseCircleOutlined className="text-danger" />
+                )}
+              </b>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  const showEachOrders = () =>
+    orders.map((order, i) => (
+      <div key={i} className="m-5 p-3 card">
+        <ShowPaymentInfo order={order} />
+        {showOrderInTable(order)}
+        <div className="row">
+          <div className="col">
+            <p>PDF Download</p>
+          </div>
+        </div>
       </div>
-      <div className="col">user history page</div>
+    ));
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <UserNav />
+        </div>
+        <div className="col text-center">
+          <h4>
+            {orders.length > 0 ? "User purchase orders" : "No purchase orders"}
+          </h4>
+          {showEachOrders()}
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default History;
