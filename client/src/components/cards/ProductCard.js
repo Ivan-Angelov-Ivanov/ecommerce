be99bbb682d900120b4bf4c3a5c3582d115412bf
Ivan Ivanov <ivan98angelov@gmail.com>
2021-02-09 11:54:57 +0200

    When product count is 0, Out of Stock

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -73,9 +73,9 @@ const ProductCard = ({ product }) => {
               <EyeOutlined className="text-info" /> <br /> View Product
             </Link>,
             <Tooltip title={tooltip}>
-              <a onClick={handleAddToCart}>
-                <ShoppingCartOutlined className="text-success" /> <br /> Add to
-                cart
+              <a onClick={handleAddToCart} disabled={product.quantity < 1}>
+                <ShoppingCartOutlined className="text-success" /> <br />
+                {product.quantity < 1 ? "Out of Stock" : "Add to cart"}
               </a>
             </Tooltip>,
           ]}
